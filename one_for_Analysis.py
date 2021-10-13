b8b7d49f329680134dc3ce10023d7aae3f927ff9
Cai Yi-Xiu <sj985517@gmail.com>
2021-10-13 10:29:57 +0800

    modify one_for_analysis.py

--- one_for_Analysis.py
+++ one_for_Analysis.py
@@ -196,8 +196,9 @@ def main():
             print("**********************************   {} / {}   **********************************\n".format(gnum + 1,
                                                                                                                count))
             print(x)
+            genome_path=os.path.join(outdir__,x)
             # python3 QualityCheck.py -r /data/usrhome/LabSSLin/user30/Desktop/RefSeq/ -g /data/usrhome/LabSSLin/user30/Desktop/SRA/test0812/assembly_result/contigs.fa -db enterobacterales_odb10 -m geno -o /data/usrhome/LabSSLin/user30/Desktop/QualityCheck
-            qual_cmd = "python3 QualityCheckv3-124.py -r {} -g {} -db {} -m {} -o {}".format(ref_dir, x, buscoDB, buscoMode,
+            qual_cmd = "python3 QualityCheckv3-124.py -r {} -g {} -db {} -m {} -o {}".format(ref_dir, genome_path, buscoDB, buscoMode,
                                                                                          outdir)
             print("run cmd: {}\n".format(qual_cmd))
 
