91d1438ea12a02a5f2e9a590c2d287bc84580947
Cai Yi-Xiu <sj985517@gmail.com>
2021-10-14 14:40:18 +0800

    updateDB

--- updateDB.py
+++ updateDB.py
@@ -24,29 +24,28 @@ def main():
         #conn = pymysql.connect(**db_settings)
     conn=pymysql.connect(host="127.0.0.1",user="root",password="tumvgk01",database="SRA_Analysis",port=3306)
     cursor=conn.cursor()
-    insert = "INSERT INTO `Final`(`Accession`, `MLST`, `AMR`, `Point`, `Serotype`, `Inc_Type`) VALUES ({},{},{},{},{},{}) ".format("12",111,"111","111","111","1111")
-    try:
-        cursor.execute(insert)
-        conn.commit()
-    except Exception as e:
-        print("ffff")
-        conn.rollback()
-        print(e)
+    #insert = "INSERT INTO `Final`(`Accession`, `MLST`, `AMR`, `Point`, `Serotype`, `Inc_Type`) VALUES ({},{},{},{},{},{}) ".format("12",111,"111","111","111","1111")
+    #try:
+    #    cursor.execute(insert)
+    #    conn.commit()
+    #except Exception as e:
+    #    print("ffff")
+     #   conn.rollback()
+     #   print(e)
     #insertSRA = "INSERT INTO SRA(Genome) VALUES(%s);"
     #insert = "INSERT INTO Final(Accession,MLST,AMR,Serotype,Inc_Type) VALUES(%s,%s,%s,%s,%s);"
-    #for i in range(0,len(df)-1):
-        #print(str(df.loc[i,"Accession"])+" "+str(df.loc[i,"mlst"])+" "+str(df.loc[i,"plasmidfinder"])+" "+str(df.loc[i,"amr_gane"])+" "+str(df.loc[i,"sistr"]))
-    #    insert = "INSERT INTO `Final`(`Accession`,`MLST`,`AMR`,`Point`,`Serotype`,`Inc_Type`) VALUES ({},{},{},NAN,{},0)".format(
-    #        str(df.loc[i, "Accession"]), int(df.loc[i, "mlst"]), str(df.loc[i, "amr_gane"]).replace(",","|"), str(df.loc[i,"sistr"]).replace(",","|")
-     #       )
-    #    try:
-    #        cursor.execute(insert)
-    #        conn.commit()
-   #         #cursor.execute(insert, (str(df.loc[i,"Accession"]),str(df.loc[i,"mlst"]),str(df.loc[i,"amr_gane"]),str(df.loc["sistr"]),str(df.loc[i,"plasmidfinder"])))
-   #     except Exception as e:
-    #        print("ffff")
-    #        conn.rollback()
-    #        print(e)
+    for i in range(0,len(df)-1):
+        print(str(df.loc[i,"Accession"])+" "+str(df.loc[i,"mlst"])+" "+str(df.loc[i,"plasmidfinder"])+" "+str(df.loc[i,"amr_gane"])+" "+str(df.loc[i,"sistr"]))
+        insert = "INSERT INTO `Final`(`Accession`, `MLST`, `AMR`, `Point`, `Serotype`, `Inc_Type`) VALUES ({},{},{},{},{},{}) ".format(
+            str(df.loc[i,"Accession"]),int(df.loc[i,"mlst"]),str(df.loc[i,"plasmidfinder"]),"111",str(df.loc[i,"amr_gane"]),str(df.loc[i,"sistr"]))
+        try:
+            cursor.execute(insert)
+            conn.commit()
+            #cursor.execute(insert, (str(df.loc[i,"Accession"]),str(df.loc[i,"mlst"]),str(df.loc[i,"amr_gane"]),str(df.loc["sistr"]),str(df.loc[i,"plasmidfinder"])))
+        except Exception as e:
+            print("ffff\n")
+            conn.rollback()
+            print(e)
     conn.close()
 
 
