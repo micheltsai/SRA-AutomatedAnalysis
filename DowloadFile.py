64a0551e7d5a1bce8f0c7328b4a3d7c33cfc3e74
Cai Yi-Xiu <sj985517@gmail.com>
2021-10-14 11:13:18 +0800

    Download

--- DowloadFile.py
+++ DowloadFile.py
@@ -41,107 +41,104 @@ def main():
     n = args.n
     utils_.progress_bar("read arguments")
 
-    try:
-        current_path = os.path.abspath(os.getcwd())
-        print("current_path: ", current_path, "\n")
-        ## read SRAsetting.txt
-        utils_.progress_bar("read SRAsetting.txt")
-        setting_path = os.path.join(current_path, "SRAsettings.txt")
-        with open(setting_path, "r") as f:
-            setList = f.readlines()
 
-        print(setList)
-        i = 0
-        for line in setList:
-            line = line.strip("\n")
-            line_ = line.split("=")
-            if line != "" and len(line_) == 2:
-                print(line_)
-                print("line{}. {}:{}\n".format(i, line_[0], line_[1]))
-            i += 1
+    current_path = os.path.abspath(os.getcwd())
+    print("current_path: ", current_path, "\n")
+    ## read SRAsetting.txt
+    utils_.progress_bar("read SRAsetting.txt")
+    setting_path = os.path.join(current_path, "SRAsettings.txt")
+    with open(setting_path, "r") as f:
+        setList = f.readlines()
 
-        outdir = setList[10].strip("\n").split("=")[1]
+    print(setList)
+    i = 0
+    for line in setList:
+        line = line.strip("\n")
+        line_ = line.split("=")
+        if line != "" and len(line_) == 2:
+            print(line_)
+            print("line{}. {}:{}\n".format(i, line_[0], line_[1]))
+        i += 1
 
-        utils_.mkdir_join(outdir)
-        pdat = date.replace("/", "")
-        outdir = os.path.join(outdir, pdat)
-        utils_.mkdir_join(outdir)
+    outdir = setList[10].strip("\n").split("=")[1]
 
-        print("output: {}\n".format(outdir))
+    utils_.mkdir_join(outdir)
+    pdat = date.replace("/", "")
+    outdir = os.path.join(outdir, pdat)
+    utils_.mkdir_join(outdir)
 
-        check_log = os.path.join(outdir, "checkDownload.log")
-        # commit
-        # run_cmd2("touch {}".format("check.log"))
-        myfile = Path(check_log)
-        myfile.touch(exist_ok=True)
-        f = open(check_log, 'a+')
-        t = str(datetime.datetime.now()).split(".")[0]
-        f.write(t)
-        f.write("\n")
-        f.close()
+    print("output: {}\n".format(outdir))
 
-        # mkdir_join(log_dir)
-        # check_log = os.path.join(log_dir, "check.log")
+    check_log = os.path.join(outdir, "checkDownload.log")
+    # commit
+    #run_cmd2("touch {}".format("check.log"))
+    myfile = Path(check_log)
+    myfile.touch(exist_ok=True)
+    f = open(check_log, 'a+')
+    t = str(datetime.datetime.now()).split(".")[0]
+    f.write(t)
+    f.write("\n")
+    f.close()
 
-        # print(date)
-        pattern, count = utils_.count_egquery(pattern, date, date)
-        print("pattern: {}\ncount: {}\n".format(pattern, count))
+    #mkdir_join(log_dir)
+    #check_log = os.path.join(log_dir, "check.log")
 
-        i_e_ = time.time()
-        idlist = utils_.IdList_esearch(pattern, 'sra', count)
 
-        print(idlist)
+    # print(date)
+    pattern, count = utils_.count_egquery(pattern, date, date)
+    print ("pattern: {}\ncount: {}\n".format(pattern,count))
 
-        runinfo = utils_.Get_RunInfo(idlist)
-        # progress_bar("get SRAfile name List stored in run_list")
-        run_list = list(runinfo['Run'])  # get SRAfile nameList stored in run_list
-        print("runinfo: {}\n run_list: {}\n".format(runinfo, run_list))
+    i_e_=time.time()
+    idlist = utils_.IdList_esearch(pattern, 'sra', count)
 
-        sra_dir = os.path.join(outdir, "sra")  # .sra file
-        utils_.mkdir_join(sra_dir)
+    print(idlist)
 
-        read_log_ = time.time()
-        f = open(check_log, 'r+')
-        d = f.readlines()
-        print("check log :{}\n".format(d))
-        f.close()
+    runinfo = utils_.Get_RunInfo(idlist)
+    #progress_bar("get SRAfile name List stored in run_list")
+    run_list = list(runinfo['Run']) #get SRAfile nameList stored in run_list
+    print("runinfo: {}\n run_list: {}\n".format(runinfo, run_list))
 
-        for s in d:
-            print("{}\n".format(s))
-        finish = list(filter(lambda x: len(x.split(" ")) >= 4, d))
-        finish_run = list(map(lambda x: x.split(" ")[1], finish))
-        need_run = list(filter(lambda x: x not in finish_run, run_list))
-        print("finish: {}\nfinish_run: {}\nneed_run".format(finish, finish_run, need_run))
-        print("finish length: {}\nfinish_run length: {}\nneed_run length: ".format(len(finish), len(finish_run),
-                                                                                   len(need_run)))
-        print("Toal", len(need_run), "sra runs need to downlaod.")
 
-        num = len(finish_run)
-        for x in need_run:
-            print("---------------------\n---------------------[ {} / {} ]---------------------\n".format(num,
-                                                                                                          len(idlist)))
-            num += 1
-            print("x = {}".format(x))
-            # outdir__ = os.path.join(output, "out")
-            outdir__ = os.path.join(outdir, "Assembled")
+    sra_dir = os.path.join(outdir, "sra")  # .sra file
+    utils_.mkdir_join(sra_dir)
 
-            final_dir = os.path.join(outdir__, "{}_contig.fa".format(x))
-            if os.path.isfile(final_dir):
-                print("was ran assembly ,contig.fa is exist\n------------------------------\n\n")
-            else:
-                utils_.prefetch_sra(x, sra_dir)
-                print("Download {}\n.".format(x))
-                with open(check_log, "a+") as f:
-                    f.write("Run {} is ok.\n".format(num, x))
-        print("Download all {}".format(date))
-    except Exception as e:
-        error_class = e.__class__.__name__  # 取得錯誤類型
-        detail = e.args[0]  # 取得詳細內容
-        cl, exc, tb = sys.exc_info()  # 取得Call Stack
-        lastCallStack = traceback.extract_tb(tb)[-1]  # 取得Call Stack的最後一筆資料
-        fileName = lastCallStack[0]  # 取得發生的檔案名稱
-        lineNum = lastCallStack[1]  # 取得發生的行號
-        funcName = lastCallStack[2]  # 取得發生的函數名稱
-        errMsg = "File \"{}\", line {}, in {}: [{}] {}".format(fileName, lineNum, funcName, error_class, detail)
-        print(errMsg)
+    read_log_=time.time()
+    f = open(check_log, 'r+')
+    d = f.readlines()
+    print("check log :{}\n".format(d))
+    f.close()
 
+
+    for s in d:
+        print ("{}\n".format(s))
+    finish = list(filter(lambda x: len(x.split(" ")) >= 4, d))
+    finish_run = list(map(lambda x: x.split(" ")[1], finish))
+    need_run = list(filter(lambda x: x not in finish_run, run_list))
+    print("finish: {}\nfinish_run: {}\nneed_run".format(finish,finish_run,need_run))
+    print("finish length: {}\nfinish_run length: {}\nneed_run length: ".format(len(finish), len(finish_run), len(need_run)))
+    print("Toal", len(need_run), "sra runs need to downlaod.")
+
+    num = len(finish_run)
+    for x in need_run:
+        print("---------------------\n---------------------[ {} / {} ]---------------------\n".format(num, len(idlist)))
+        num += 1
+        print("x = {}".format(x))
+        # outdir__ = os.path.join(output, "out")
+        outdir__ = os.path.join(outdir, "Assembled")
+        final_dir = os.path.join(outdir__, "{}_contig.fa".format(x))
+        if os.path.isfile(final_dir):
+            print("was ran assembly ,contig.fa is exist\n------------------------------\n\n")
+        else:
+            utils_.prefetch_sra(x, sra_dir)
+            print("Download {}\n.".format(x))
+            with open(check_log,"a+") as f:
+                f.write("Run {} is ok.\n".format(num,x))
+
+
+
+
+
+    print("Download all {}".format(date))
+
+if __name__ == '__main__':
+    main()
