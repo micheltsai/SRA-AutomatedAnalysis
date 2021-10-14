8d1f37402982edf9d0a7972ec660e25a696ea8ec
Cai Yi-Xiu <sj985517@gmail.com>
2021-10-14 13:26:31 +0800

    update

--- updateDB.py
+++ updateDB.py
@@ -22,15 +22,23 @@ def main():
     try:
         conn = pymysql.connect(**db_settings)
 
-        with conn.cursor() as cursor:
-            #insertSRA = "INSERT INTO SRA(Genome) VALUES(%s);"
-            insert = "INSERT INTO Final(Accession,MLST,AMR,Serotype,Inc_Type) VALUES(%s,%s,%s,%s,%s);"
-            for i in range(len(df)):
-                print(str(df.loc[i,"Accession"])+" "+str(df.loc[i,"mlst"])+" "+str(df.loc[i,"plasmidfinder"])+" "+str(df.loc[i,"amr_gane"])+" "+str(df.loc["sistr"]))
-                cursor.execute(insert, (str(df.loc[i,"Accession"]),str(df.loc[i,"mlst"]),str(df.loc[i,"amr_gane"]),str(df.loc["sistr"]),str(df.loc[i,"plasmidfinder"])))
-
+        cursor=conn.cursor()
+        #insertSRA = "INSERT INTO SRA(Genome) VALUES(%s);"
+        #insert = "INSERT INTO Final(Accession,MLST,AMR,Serotype,Inc_Type) VALUES(%s,%s,%s,%s,%s);"
+        for i in range(len(df)):
+            print(str(df.loc[i,"Accession"])+" "+str(df.loc[i,"mlst"])+" "+str(df.loc[i,"plasmidfinder"])+" "+str(df.loc[i,"amr_gane"])+" "+str(df.loc["sistr"]))
+            insert = "INSERT INTO Final(Accession,MLST,AMR,Serotype,Inc_Type) VALUES({},{},{},{},{});".format(
+                str(df.loc[i, "Accession"]), str(df.loc[i, "mlst"]), str(df.loc[i, "amr_gane"]), str(df.loc["sistr"]),
+                str(df.loc[i, "plasmidfinder"]))
+            cursor.execute(insert)
+            conn.commit()
+            #cursor.execute(insert, (str(df.loc[i,"Accession"]),str(df.loc[i,"mlst"]),str(df.loc[i,"amr_gane"]),str(df.loc["sistr"]),str(df.loc[i,"plasmidfinder"])))
+        data=cursor.fetchone()
+        print("Database version: %s"%data)
+        conn.close()
     except Exception as e:
         print("ffff")
+        conn.rollback()
         print(e)
 
 
