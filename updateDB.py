de0205c29b5200f678bf91c5ff3d6d21243c9bc6
Cai Yi-Xiu <sj985517@gmail.com>
2021-10-14 14:27:44 +0800

    updateDB

--- updateDB.py
+++ updateDB.py
@@ -26,21 +26,28 @@ def main():
     cursor=conn.cursor()
     insert = "INSERT INTO `Final`(`Accession`,`MLST`,`AMR`,`Point`,`Serotype`,`Inc_Type`) VALUES ({},{},{},0,{},{})".format(
         "aaa","aaa","aaa","aaa","aaa")
+    try:
+        cursor.execute(insert)
+        conn.commit()
+    except Exception as e:
+        print("ffff")
+        conn.rollback()
+        print(e)
     #insertSRA = "INSERT INTO SRA(Genome) VALUES(%s);"
     #insert = "INSERT INTO Final(Accession,MLST,AMR,Serotype,Inc_Type) VALUES(%s,%s,%s,%s,%s);"
-    for i in range(0,len(df)-1):
+    #for i in range(0,len(df)-1):
         #print(str(df.loc[i,"Accession"])+" "+str(df.loc[i,"mlst"])+" "+str(df.loc[i,"plasmidfinder"])+" "+str(df.loc[i,"amr_gane"])+" "+str(df.loc[i,"sistr"]))
-        insert = "INSERT INTO `Final`(`Accession`,`MLST`,`AMR`,`Point`,`Serotype`,`Inc_Type`) VALUES ({},{},{},NAN,{},0)".format(
-            str(df.loc[i, "Accession"]), int(df.loc[i, "mlst"]), str(df.loc[i, "amr_gane"]).replace(",","|"), str(df.loc[i,"sistr"]).replace(",","|")
-            )
-        try:
-            cursor.execute(insert)
-            conn.commit()
-            #cursor.execute(insert, (str(df.loc[i,"Accession"]),str(df.loc[i,"mlst"]),str(df.loc[i,"amr_gane"]),str(df.loc["sistr"]),str(df.loc[i,"plasmidfinder"])))
-        except Exception as e:
-            print("ffff")
-            conn.rollback()
-            print(e)
+    #    insert = "INSERT INTO `Final`(`Accession`,`MLST`,`AMR`,`Point`,`Serotype`,`Inc_Type`) VALUES ({},{},{},NAN,{},0)".format(
+    #        str(df.loc[i, "Accession"]), int(df.loc[i, "mlst"]), str(df.loc[i, "amr_gane"]).replace(",","|"), str(df.loc[i,"sistr"]).replace(",","|")
+     #       )
+    #    try:
+    #        cursor.execute(insert)
+    #        conn.commit()
+   #         #cursor.execute(insert, (str(df.loc[i,"Accession"]),str(df.loc[i,"mlst"]),str(df.loc[i,"amr_gane"]),str(df.loc["sistr"]),str(df.loc[i,"plasmidfinder"])))
+   #     except Exception as e:
+    #        print("ffff")
+    #        conn.rollback()
+    #        print(e)
     conn.close()
 
 
