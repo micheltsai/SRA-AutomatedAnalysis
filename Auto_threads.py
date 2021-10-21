7be5fea8a613968521add790c153e20fac65838f
Cai Yi-Xiu <sj985517@gmail.com>
2021-10-21 14:56:24 +0800

    add threads

--- Auto_threads.py
+++ Auto_threads.py
@@ -8,6 +8,7 @@ import subprocess
 import sys
 import time
 import traceback
+from pathlib import Path
 
 import utils_
 
@@ -43,10 +44,9 @@ def progress_bar(Category):
         time.sleep(0.02)
     print ("\n")
 
-def Download(date):
-    print("Dowload threadPID",os.getgid())
-    print("Download 父進程編號",os.getppid())
-    run_cmd("python3 DownloadFile.py --PDAT {}".format(date))
+
+
+
 def Analysis(date):
     print("Analysis threadPID", os.getgid())
     print("Analysis 父進程編號", os.getppid())
@@ -56,25 +56,113 @@ def Analysis(date):
 if __name__ == '__main__':
     start=time.time()
     Month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    current_path = os.path.abspath(os.getcwd())
+    print("current_path: ", current_path, "\n")
+
+    ## read SRAsetting.txt
+    utils_.progress_bar("read SRAsetting.txt")
+    setting_path = os.path.join(current_path, "SRAsettings.txt")
+    with open(setting_path, "r") as f:
+        setList = f.readlines()
+
+    print(setList)
+    i = 0
+    for line in setList:
+        line = line.strip("\n")
+        line_ = line.split("=")
+        if line != "" and len(line_) == 2:
+            print(line_)
+            print("line{}. {}:{}\n".format(i, line_[0], line_[1]))
+        i += 1
+    outdir = setList[10].strip("\n").split("=")[1]
+    utils_.mkdir_join(outdir)
     try:
         for x in range(0, 12):
             for d in range(1, Month[x] + 1):
                 ds = time.time()
                 c = datetime.datetime(2020, x + 1, d)
-                tmp = c.strftime("%Y/%m/%d")
-                download_prog=multiprocessing.Process(target=Download, args=(tmp,))
-                download_prog2 = multiprocessing.Process(target=Download, args=(tmp,))
-                #analysis_prog=multiprocessing.Process(target=Analysis, args=(tmp,))
+                date = c.strftime("%Y/%m/%d")
+                ######
+                pdat = date.replace("/", "")
+                new_outdir = os.path.join(outdir, pdat)
+                utils_.mkdir_join(new_outdir)
+                print("output: {}\n".format(new_outdir))
 
-                download_prog.start()
-                download_prog2.start()
-                progress_bar("Download")
+                check_log = os.path.join(new_outdir, "checkDownload.log")
+                # commit
+                # run_cmd2("touch {}".format("check.log"))
+                myfile = Path(check_log)
+                myfile.touch(exist_ok=True)
+                f = open(check_log, 'a+')
+                t = str(datetime.datetime.now()).split(".")[0]
+                f.write(t)
+                f.write("\n")
+                f.close()
 
-                #analysis_prog.start()
-                #progress_bar("Download")
+                # mkdir_join(log_dir)
+                # check_log = os.path.join(log_dir, "check.log")
+
+                # print(date)
+                pattern, count = utils_.count_egquery(pattern, date, date)
+                print("pattern: {}\ncount: {}\n".format(pattern, count))
+
+                i_e_ = time.time()
+                idlist = utils_.IdList_esearch(pattern, 'sra', count)
+
+                print(idlist)
+
+                runinfo = utils_.Get_RunInfo(idlist)
+                # progress_bar("get SRAfile name List stored in run_list")
+                run_list = list(runinfo['Run'])  # get SRAfile nameList stored in run_list
+                print("runinfo: {}\n run_list: {}\n".format(runinfo, run_list))
+
+                sra_dir = os.path.join(outdir, "sra")  # .sra file
+                utils_.mkdir_join(sra_dir)
+
+                read_log_ = time.time()
+                f = open(check_log, 'r+')
+                line = f.readlines()
+                print("check log :{}\n".format(line))
+                f.close()
+
+                for s in line:
+                    print("{}\n".format(s))
+                finish = list(filter(lambda x: len(x.split(" ")) >= 4, line))
+                finish_run = list(map(lambda x: x.split(" ")[1], finish))
+                need_run = list(filter(lambda x: x not in finish_run, run_list))
+                print("finish: {}\nfinish_run: {}\nneed_run".format(finish, finish_run, need_run))
+                print("finish length: {}\nfinish_run length: {}\nneed_run length: ".format(len(finish), len(finish_run),
+                                                                                           len(need_run)))
+                print("Toal", len(need_run), "sra runs need to downlaod.")
+
+                num = len(finish_run)
+                for x in need_run:
+                    one_ = time.time()
+                    print(
+                        "---------------------\n---------------------[ {} / {} ]---------------------\n".format(num + 1,
+                                                                                                                len(idlist)))
+                    num += 1
+                    print("x = {}".format(x))
+                    # outdir__ = os.path.join(output, "out")
+                    outdir__ = os.path.join(new_outdir, "Assembled")
+
+                    final_dir = os.path.join(outdir__, "{}_contig.fa".format(x))
+                    if os.path.isfile(final_dir):
+                        print("was ran assembly ,contig.fa is exist\n------------------------------\n\n")
+                    else:
+                        utils_.prefetch_sra(x, sra_dir)
+                        print("Download {}\n.".format(x))
+                        with open(check_log, "a+") as f:
+                            f.write("{}\n".format(x))
+                    print('Done,total cost', time.time() - one_, 'secs')
+
+                print("Download all {}".format(date))
+                print('Done,total cost', time.time() - start, 'secs')
+                ##########
+
 
                 with open("./Automate_check.log", "a+") as f:
-                    f.write("{}:{}:{}\n".format(tmp, time.time() - ds, time.time() - start))
+                    f.write("{}:{}:{}\n".format(date, time.time() - ds, time.time() - start))
     except Exception as e:
         error_class = e.__class__.__name__  # 取得錯誤類型
         detail = e.args[0]  # 取得詳細內容
