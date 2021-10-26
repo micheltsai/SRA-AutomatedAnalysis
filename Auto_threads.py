16fafcd00ccfa4618b7450ccd141a203abcfc511
Cai Yi-Xiu <sj985517@gmail.com>
2021-10-26 10:58:54 +0800

    modify settings

--- Auto_threads.py
+++ Auto_threads.py
@@ -687,7 +687,7 @@ if __name__ == '__main__':
         i += 1
     print(settings_dict)
     #setting_df=pd.DataFrame(settings_dict)
-    setting_df-pd.DataFrame.from_dict(settings_dict,orient='index').T
+    setting_df=pd.DataFrame.from_dict(settings_dict,orient='index').T
     print(setting_df)
 
 
